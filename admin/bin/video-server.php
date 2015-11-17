d9563f46b9f854547c45c63ecc434a371e5fb8d4
Everton J Paula <everton.projetos@gmail.com>
2015-11-16 22:25:56 -0200

    Modificações Chat e Elaborada Base video conferência
    Elaborado a base da video conferência, faltando apenas a aplicação no
    modelo, e mudança de participantes no select do controler para
    restringir participantes do mesmo grupo.

---
+++ admin/bin/video-server.php
@@ -0,0 +1,11 @@
+<?php
+/* $ php bin/chat-server.php *///comando Shell de ativação
+use Video\Video as Video;
+use Ratchet\App as App;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+// Your shell script
+$server = new App("localhost");
+$server->route('/Video',new Video);
+$server->run();
